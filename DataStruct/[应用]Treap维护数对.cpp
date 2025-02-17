666ee40a7df62e7e43dca3343b9ba6d5d07ee220
Codecsystem <codesystem0471@gmail.com>
2025-02-17 20:46:10 +0800

    update Treap

--- DataStruct/[应用]Treap维护数对.cpp
+++ DataStruct/[应用]Treap维护数对.cpp
@@ -20,15 +20,15 @@ class FHQTreap{
     //树堆：BST+Heap
     public:
         struct Node{
-            int val;
+            int l,r;
             int size=0;
             int priority;//随机数
             Node *left, *right;
-            Node(int val):val(val),priority(rand()),left(NULL),right(NULL),size(1){}
-            Node(int val,int priority):val(val),priority(priority),left(NULL),right(NULL),size(1){}
+            Node(int l,int r):l(l),r(r),priority(rand()),left(NULL),right(NULL),size(1){}
         };
-        bool cmp(Node a,Node b){
-            return a.val<b.val;
+        bool cmp(Node *a,pair<int,int> val){
+            if(a->l==val.first) return a->r<=val.second;
+            return a->l<val.first;
         }
         Node *root;
         FHQTreap():root(NULL){}
@@ -54,14 +54,14 @@ class FHQTreap{
                 //cout<<root->val<<' '<<root->size<<endl;
             }
         }
-        void split(Node *root,Node *&a,Node *&b,int val){
+        void split(Node *root,Node *&a,Node *&b,pair<int,int> val){
             //将root按照val分割为a,b两部分
             //a的val都小于等于val，b的val都大于val
             if(!root){
                 a=b=NULL;
                 return;
             }
-            if(root->val<=val){
+            if(cmp(root,val)){
                 a=root;
                 split(root->right,a->right,b,val);
             }
@@ -77,17 +77,17 @@ class FHQTreap{
                 //cout<<root->val<<' '<<root->size<<endl;
             }
         }
-        void insert(int val){
+        void insert(pair<int,int> val){
             Node *a,*b;
             split(root,a,b,val);//将root按照val分割为a,b两部分
-            merge(a,a,new Node(val));//将val插入到a中
+            merge(a,a,new Node(val.first,val.second));//将val插入到a中
             merge(root,a,b);//将a,b合并为root
             //偶还能这样
         }
-        void erase(int val){
+        void erase(pair<int,int> val){
             Node *a,*b,*c;
             split(root,a,b,val);//将root按照val分割为a,b两部分
-            split(a,a,c,val-1);//将a按照val-1分割为a,c两部分
+            split(a,a,c,{val.first,val.second-1});//将a按照val-1分割为a,c两部分
             if(c)
             {
                 merge(a,a,c->right);//将c的右子树合并到a中(删除一个节点)
@@ -95,100 +95,116 @@ class FHQTreap{
             }
             merge(root,a,b);//将a,b合并为root
         }
-        void print(Node *root){
-            if(!root)return;
-            print(root->left);
-            cout<<root->val<<" ";
-            print(root->right);
-        }
-        int findMax(Node *root){
-            if(!root)return -1;
+        pair<int,int> findMax(Node *root){
+            if(!root)return {-1,-1};
             while(root->right)root=root->right;
-            return root->val;
+            return {root->l,root->r};
         }
-        int findMin(Node *root){
-            if(!root)return -1;
+        pair<int,int> findMin(Node *root){
+            if(!root)return {-1,-1};
             while(root->left)root=root->left;
-            return root->val;
+            return {root->l,root->r};
         }
-        int pre(int val){
+        pair<int,int> pre(pair<int,int> val){
             Node *a,*b;
-            split(root,a,b,val-1);//将root按照val-1分割为a,b两部分
-            int res=findMax(a);
+            split(root,a,b,{val.first,val.second-1});//将root按照val-1分割为a,b两部分
+            pair<int,int> res=findMax(a);
             merge(root,a,b);
             return res;
         }
-        int next(int val){
-            Node *a,*b;
-            split(root,a,b,val);//将root按照val分割为a,b两部分
-            int res=findMin(b);
-            merge(root,a,b);
-            return res;
-        }
-        int rank(int val){
-            Node *a,*b;
-            split(root,a,b,val-1);//将root按照val-1分割为a,b两部分
-            int res=(a?a->size:0)+1;
-            merge(root,a,b);
-            return res;
-        }
-        int QueryKth(int k){
-            return KthQuery(root,k);
-        }
-        int KthQuery(Node* root,int k){
-            if(root==nullptr) return -1;
-            int leftsize=root->left?root->left->size:0;
-            if(k<=leftsize) return KthQuery(root->left,k);
-            else if(k==leftsize+1) return root->val;
-            else return KthQuery(root->right,k-leftsize-1);
-        }
-        bool find(int val){
+        pair<int,int> next(pair<int,int> val){
             Node *a,*b;
             split(root,a,b,val);//将root按照val分割为a,b两部分
-            bool res=a&&findMax(a)==val;
+            pair<int,int> res=findMin(b);
             merge(root,a,b);
             return res;
         }
+		bool find(pair<int,int> val)
+		{
+			Node *a,*b;
+			split(root,a,b,val);
+			bool res=a&&findMax(a).second==val.second&&findMax(a).first==val.first;
+			merge(root,a,b);
+			return res;
+		}
+		int size()
+		{
+			return root?root->size:0;
+		}
         
 };
+int read()
+{
+	int s=0,f=1;
+	char ch=getchar();
+	while(ch<'0'||ch>'9')
+	{
+		if(ch=='-') f=-1;
+		ch=getchar();
+	}
+	while(ch>='0'&&ch<='9')
+	{
+		s=(s<<3)+(s<<1)+ch-'0';
+		ch=getchar();
+	}
+	return s*f;
+}
+inline void write(int x) 
+{
+	static int sta[35]; 
+	int top=0;
+	if(x<0&&x!=-2147483648) {putchar('-');x=-x;}
+	if(x==-2147483648) {printf("-2147483648");return;}
+	do{
+	  sta[top++]=x%10, x/=10;
+	  }while(x);
+	while(top) putchar(sta[--top]+48);
+}
 int main()
 {
-    int T_start=clock();
-    FHQTreap treap;
-    vector<int> test={1,2,3,4,5,6,7,8,9,10};
-    /*for(int i=0;i<test.size();i++){
-        treap.insert(test[i]);
+	//freopen("in.txt","r",stdin);
+	//freopen("out2.txt","w",stdout);
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+    int t;cin>>t;
+	FHQTreap tree;
+    while(t--)
+    {
+        char op;
+		cin>>op;
+		if(op=='B')
+		{
+			cout<<tree.size()<<'\n';
+		}
+		else if(op=='A')
+		{
+			int x,y;
+			cin>>x>>y;
+			auto check=[](pair<int,int> a,pair<int,int> b)->bool{
+				vector<int> v1={a.first,a.second,b.first,b.second};
+				sort(v1.begin(),v1.end());
+				return ((v1[0]==a.first&&v1[1]==a.second&&v1[2]==b.first&&v1[3]==b.second)||(v1[0]==b.first&&v1[1]==b.second&&v1[2]==a.first&&v1[3]==a.second))&&v1[1]!=v1[2];
+			};
+			int ans=0;
+			if(tree.find({x,y}))
+			{
+				tree.erase({x,y});
+				ans++;
+			}
+			while(tree.size()&&tree.pre({x,y}).first!=-1&&!check(tree.pre({x,y}),{x,y})) 
+			{
+				ans++;
+				tree.erase(tree.pre({x,y}));
+			}
+			while(tree.size()&&tree.next({x,y}).first!=-1&&!check(tree.next({x,y}),{x,y})) 
+			{
+				ans++;
+				tree.erase(tree.next({x,y}));
+			}
+			cout<<ans<<'\n';
+			tree.insert({x,y});
+		}
     }
-    treap.print(treap.root);
-    cout<<endl;
-    treap.erase(5);
-    treap.print(treap.root);
-    cout<<endl;
-    treap.insert(5);
-    treap.insert(5);
-    treap.insert(5);
-    treap.insert(5);
-    treap.print(treap.root);
-    cout<<endl;
-    treap.erase(5);
-    treap.print(treap.root);
-    cout<<endl;*/
-    cout<<treap.pre(5)<<endl;
-    cout<<treap.next(5)<<endl;
-    cout<<treap.rank(5)<<endl;
-    treap.erase(5);
-    treap.print(treap.root);
-    cout<<endl;
-    treap.insert(5);
-    treap.insert(5);
-    treap.insert(5);
-    treap.print(treap.root);
-    cout<<endl;
-    cout<<treap.rank(6)<<endl;
-    treap.erase(5);
-    treap.print(treap.root);
-    cout<<endl;
-    cout<<treap.rank(8)<<endl;
-    cout<<treap.QueryKth(7)<<endl;
     return 0;
 }
