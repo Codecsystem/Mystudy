6fcbdc8f5657fa29a5374040b03aeedc2d6012b7
Codecsystem <codesystem0471@gmail.com>
2025-03-07 00:44:00 +0800

    upd shortestpath

---
+++ Graph/ShortestPath（Bellman_Ford,SPFA).cpp
@@ -0,0 +1,119 @@
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <map>
+#include <iostream>
+#include <queue>
+#include <set>
+#include <stack>
+#include <vector>
+#include <array>
+#include <unordered_map>
+using namespace std;
+int read()
+{
+    int s=0,f=1;
+    char ch=getchar();
+    while(ch<'0'||ch>'9')
+    {
+        if(ch=='-') f=-1;
+        ch=getchar();
+    }
+    while(ch>='0'&&ch<='9')
+    {
+        s=(s<<3)+(s<<1)+ch-'0';
+        ch=getchar();
+    }
+    return s*f;
+}
+inline void write(int x) 
+{
+    static int sta[35]; 
+    int top=0;
+    if(x<0&&x!=-2147483648) {putchar('-');x=-x;}
+    if(x==-2147483648) {printf("-2147483648");return;}
+    do{
+      sta[top++]=x%10, x/=10;
+      }while(x);
+    while(top) putchar(sta[--top]+48);
+}
+vector<int> Bellman_Ford(vector<vector<pair<int,int>>>& mp,int s,int n)
+{
+    vector<int> dis(n+1,0x7fffffff);
+    dis[s]=0;
+    for(int i=1;i<=n-1;i++)
+    {
+        for(int j=1;j<=n;j++)
+        {
+            for(auto [u,w]:mp[j])
+            {
+                if(dis[j]!=0x7fffffff&&dis[j]+w<dis[u])
+                {
+                    dis[u]=dis[j]+w;
+                }
+            }
+        }     
+    }
+    for(int j=1;j<=n;j++)
+    {
+        for(auto [u,w]:mp[j])
+        {
+            if(dis[j]!=0x7fffffff&&dis[j]+w<dis[u])
+            {
+                cout<<"negative cycle!"<<endl;
+            }
+        }
+    }
+    return dis;
+    //Bellman_Ford 对所有的边进行n-1次松弛操作，如果在进行第n次松弛操作时，仍然存在边可以松弛，则说明图中存在负权环（从s点出发存在负权环）
+    //时间复杂度：O(nm),形式上就是暴力）
+    //第i次循环，我们能找到经历i条边到达的点的最短距离
+    //所以第n次循环，我们能找到经历n条边到达的点的最短距离，如果存在负权环，那么一定能在第n次循环找到经历n条边到达的点的最短距离
+}
+vector<int> SPFA(vector<vector<pair<int,int>>>& mp,int s,int n)
+{
+    vector<int> dis(n+1,0x7fffffff);
+    vector<int> vis(n+1,0);
+    vector<int> cnt(n+1,0);
+    dis[s]=0;queue<int> q;
+    q.push(s);vis[s]=1;
+    while(!q.empty())
+    {
+        int u=q.front();
+        q.pop();vis[u]=0;
+        for(auto [v,w]:mp[u])
+        {
+            if(dis[v]>dis[u]+w)//松弛
+            {
+                dis[v]=dis[u]+w;
+                if(!vis[v])
+                {
+                    q.push(v);
+                    vis[v]=1;
+                }
+            }
+        }
+    }
+    return dis;
+    //SPFA: 形式上Bellman_Ford是一棵树，很显然，只有上一次被松弛的节点u，才有可能对v进行松弛，所以可以采用SPFA
+    //为啥只有上一次被松弛的节点u，才有可能对v进行松弛？
+    //手玩一下就好了（悲
+    //考虑简单图，他可以是个递推的过程
+}
+int main()
+{
+    int T_start=clock();
+    int n=read(),m=read();
+    vector<vector<pair<int,int>>> mp(n+1);
+    for(int i=0;i<m;i++)
+    {
+        int u=read(),v=read(),w=read();
+        mp[u].push_back({v,w});
+    }
+    return 0;
+}
