3bfe34dbaab7fdbbcc5a957ecfedda864a2afb42
Codecsystem <codesystem0471@gmail.com>
2025-07-12 13:28:24 +0800

    upd MCMF

---
+++ Graph/最小费用最大流(原始对偶).cpp
@@ -0,0 +1,136 @@
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <map>
+#include <iostream>
+#include <queue>
+#include <set>
+#include <stack>
+#include <vector>
+#include <array>
+#include <unordered_map>
+#include <numeric>
+#include <functional>
+using namespace std;
+#define int long long
+class Mcmf{
+public:
+    struct node{
+        int to;
+        int cap;
+        int cost;
+        int rev;
+    };
+    int n,s,t;
+    int maxf=0,minc=0;
+    const int INF=1e18;
+    vector<vector<node>> mp;
+    vector<int> dis,h,prev,previd;
+    Mcmf(int n,int s,int t,vector<array<int,4>>& eds):
+    n(n),s(s),t(t),mp(n+1),dis(n+1),
+    h(n+1,INF),prev(n+1),previd(n+1){
+        for(auto [u,v,cap,w]:eds){
+            int uid=mp[u].size();
+            int vid=mp[v].size();
+            mp[u].push_back({v,cap,w,vid});
+            mp[v].push_back({u,0,-w,uid});
+            //反边的费用是负的
+        }
+    }
+    bool dijk(){
+        fill(dis.begin(),dis.end(),INF);
+        dis[s]=0;
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        pq.push({0,s});
+        while(!pq.empty()){
+            auto [d,u]=pq.top();
+            pq.pop();
+            if(dis[u]<d) continue;
+            for(int i=0;i<mp[u].size();i++){
+                auto [v,cap,w,rev]=mp[u][i];
+                int cost=w+h[u]-h[v];
+                if(cap>0&&dis[u]+cost<dis[v]){
+                    dis[v]=dis[u]+cost;
+                    prev[v]=u;//记录前驱
+                    previd[v]=i;//记录当前弧
+                    pq.push({dis[v],v});
+                }
+            }
+        }
+        return dis[t]<INF;
+    }
+
+    void SPFA(){
+        h[s]=0;
+        queue<int> q;
+        vector<bool> inq(n+1,0);
+        q.push(s),inq[s]=1;
+        while(!q.empty()){
+            int u=q.front();
+            q.pop();
+            inq[u]=0;
+            for(auto [v,cap,w,rev]:mp[u]){
+                if(cap>0&&h[u]+w<h[v]){
+                    h[v]=h[u]+w;
+                    if(!inq[v]){
+                        q.push(v);
+                        inq[v]=1;
+                    }
+                }
+            }
+        }
+    }
+
+    void PD(){
+        SPFA();
+        while(dijk())
+        {
+            //now dis(u-v)=dis(u,v)(real)+h[u]-h[v]
+            //when u=0, dis(u,v)=dis(u,v)(real)-h[v]
+            int d=INF;
+            for(int i=t;i!=s;i=prev[i])
+            {
+                d=min(d,mp[prev[i]][previd[i]].cap);
+            }
+            //计算增广路径上的最小流量
+            maxf+=d;
+            minc+=d*(dis[t]+h[t]);
+            for(int i=t;i!=s;i=prev[i])
+            {
+                mp[prev[i]][previd[i]].cap-=d;
+                mp[i][mp[prev[i]][previd[i]].rev].cap+=d;
+            }
+            //更新残余网络
+            for(int i=1;i<=n;i++)
+            {
+                if(dis[i]<INF)
+                {
+                    h[i]+=dis[i];
+                }
+            }
+        }
+    }
+};
+signed main()
+{
+    int T_start=clock();
+    int n,m,s,t;
+    cin>>n>>m>>s>>t;
+    vector<array<int,4>> eds;
+    for(int i=0;i<m;i++){
+        int u,v,cap,w;
+        cin>>u>>v>>cap>>w;
+        eds.push_back({u,v,cap,w});
+    }
+    Mcmf mcmf(n,s,t,eds);
+    mcmf.PD();
+    cout<<mcmf.maxf<<" "<<mcmf.minc<<endl;
+    return 0;
+}
+//Primal-Dual 原始对偶算法,O(F*E*logE)
+//利用johnson最短路，将每条边的权值加上一个常数，使得每条边的权值非负，从而可以使用dijkstra算法
