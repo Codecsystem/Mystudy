bdd88029c487470b54699b938399072f07f19b57
Codecsystem <codesystem0471@gmail.com>
2025-08-08 21:51:07 +0800

    upd 虚树

---
+++ Graph/虚树(倍增,lca).cpp
@@ -0,0 +1,148 @@
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <map>
+#include <iostream>
+#include <queue>
+#include <set>
+#include <stack>
+#include <vector>
+#include <array>
+#include <unordered_map>
+#include <numeric>
+#include <functional>
+#include <ranges>
+#include <iomanip>
+#define int long long //赫赫 要不要龙龙呢
+using namespace std;
+class vtree{
+    public:
+    vector<vector<array<int,2>>> tr,vtr;
+    vector<vector<int>> len,fa;
+    vector<int> dfn,dep;
+    int n,k,tot;
+    vtree(int n,vector<vector<array<int,2>>>& tr):
+    tr(tr),vtr(n+1),n(n),k(__lg(n)+1),dfn(n+1,0),dep(n+1,0),tot(0),
+    fa(n+1,vector<int>(__lg(n)+2,0)),len(n+1,vector<int>(__lg(n)+2,1e18)){
+        dfs(1,0);
+    }
+
+    void dfs(int u,int f){
+        dfn[u]=++tot;
+        dep[u]=dep[f]+1;
+        fa[u][0]=f;
+        for(int i=1;i<=k;i++){ 
+            fa[u][i]=fa[fa[u][i-1]][i-1]; 
+            len[u][i]=min(len[u][i-1],len[fa[u][i-1]][i-1]);
+            
+        }
+        for(auto [v,w]:tr[u]){
+            if(v==f) continue;
+            len[v][0]=w;
+            dfs(v,u);
+        }
+    }
+
+    int lca(int u,int v){
+        if(dep[u]<dep[v]) swap(u,v);
+        for(int i=k;i>=0;i--){
+            if(dep[fa[u][i]]>=dep[v]) u=fa[u][i];
+        }
+        if(u==v) return u;
+        for(int i=k;i>=0;i--){
+            if(fa[u][i]!=fa[v][i]){
+                u=fa[u][i];
+                v=fa[v][i];
+            }
+        }
+        return fa[u][0];
+    }
+
+    int w(int u,int v){
+        if(dep[u]<dep[v]) swap(u,v);
+        int ans=1e18;
+        for(int i=k;i>=0;i--){
+            if(dep[fa[u][i]]>=dep[v]){
+                ans=min(ans,len[u][i]);
+                u=fa[u][i];
+            }
+        }
+        return ans;
+    }
+    void getvTree(vector<int>& o){
+        sort(o.begin(),o.end(),[&](int a,int b){return dfn[a]<dfn[b];});
+        int p=o.size();
+        for(int i=1;i<p;i++){
+            o.push_back(lca(o[i-1],o[i]));
+        }
+        sort(o.begin(),o.end(),[&](int a,int b){return dfn[a]<dfn[b];});
+        o.erase(unique(o.begin(),o.end()),o.end());
+        for(int i=1;i<o.size();i++){
+            int tp=lca(o[i-1],o[i]);
+            vtr[tp].push_back({o[i],w(o[i],tp)});
+            //vtr[o[i]].push_back({tp,w(o[i],tp)});
+        }
+    }
+    void clear(vector<int>& o){
+        for(auto x:o) vtr[x].clear();
+    }
+};
+signed main()
+{
+    int T_start=clock();
+    //freopen("in.txt","r",stdin);
+    //freopen("out.txt","w",stdout);
+    ios::sync_with_stdio(false),cin.tie(0),cout.tie(0);
+    int n;cin>>n;
+    vector<vector<array<int,2>>> tr(n+1);
+    for(int i=1;i<n;i++){
+        int u,v,w;cin>>u>>v>>w;
+        tr[u].push_back({v,w});
+        tr[v].push_back({u,w});
+    }
+    vtree vt(n,tr);
+    vector<bool> iskey(n+1,0);
+    int q;cin>>q;
+    while(q--){
+        vector<int> o,co;
+        int k;cin>>k;
+        for(int i=0;i<k;i++){
+            int x;cin>>x;
+            o.push_back(x);
+            iskey[x]=1;
+        }
+        co=o;
+        vt.getvTree(o);
+        auto dp=[&](this auto& self,int u)->int{
+            if(iskey[u]){
+                return vt.len[u][0];
+            }
+            int ans=0;
+            for(auto [v,w]:vt.vtr[u]){
+                ans += min(self(v), w);
+            }
+            return ans;
+        };
+        int fin=dp(o[0]);
+        if(o[0]==1)  cout<<fin<<endl;
+        else cout<<min(fin,vt.w(o[0],1))<<endl;
+        vt.clear(o);
+        for(auto x:co) iskey[x]=0;
+    }
+    return 0;
+}
+//虚树，处理q次询问，每次询问给出k个关键点，答案只跟关键点有关的问题
+//构建虚树：
+//1.将关键点按dfn排序
+//2.相邻的关键点的lca加入虚树
+//why?虚树的定义是关键点的集合和其两两lca构成的树
+//考虑任意两个关键点，其lca一定把这两个关键点分成两个分支，
+//而根据dfn的连续性，一定有某个相邻的关键点的lca是这个lca
+//Q.E.D
+//3.按父子关系构建虚树
+//时间复杂度：O(nlogn)
